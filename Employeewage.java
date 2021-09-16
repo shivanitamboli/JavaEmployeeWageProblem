36391c65fb22503b42e8007de3e9647bd8da05c1
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-16 14:30:30 +0530

    daily wage

--- Employeewage.java
+++ Employeewage.java
@@ -1,21 +1,23 @@
 package UC;
 
 public class Employeewage {
-
 	public static void main(String[] args) {
-    int IS_PRESENT = 0;
-          System.out.println("welcome to employee wage problem practice set");
-    double empCheck = Math.floor(Math.random()* 10) %2;
-    // Using random class to Get random number
-    // math.floor returns largest double value
-    if (empCheck == IS_PRESENT)
-    {
-        System.out.println("Employee is Present");
-    }
-    else 
-    {
-        System.out.println("Employee is Absent");
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empwage = 0;
+        double empCheck = Math.floor(Math.random()* 10) %2;
+    if (empCheck == IS_FULL_TIME)
+       {
+         empHrs = 8;
+       }
+    else
+       {
+         empHrs = 0;
+       }
+      empwage = empHrs*EMP_RATE_PER_HOUR; 
+      System.out.println("Emp Wage :" + empwage);
     }
 	}
 
-}
+
