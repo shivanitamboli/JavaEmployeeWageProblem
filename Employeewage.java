a1c30b8b48f3de2d7de2c5ca122057fb8feb16cb
[shivani tamboli] <[tambolishivani4@gmail.com]>
2021-09-16 15:04:03 +0530

    wage of month 100 working days

--- Employeewage.java
+++ Employeewage.java
@@ -1,16 +1,17 @@
 package UC;
 
 public class Employeewage {
-	public static final int IS_FULL_TIME = 2;
-    public static final int IS_PART_TIME = 1;
-    public static final int EMP_WAGE_PER_HOUR = 20;
-    public static final int WORKING_DAYS = 20;
+	public static final int IS_FULL_TIME=2;
+    public static final int IS_PART_TIME=1;
+    public static final int EMP_WAGE_PER_HOUR=20;
+    public static final int WORKING_DAYS=20;
     public static int Totalwage=0;
-
+    public static int Total_Working_Hours_Till_Now=0;
     public static void main(String[] args) {
         int WorkHours = 0;
-        int Day;
-        for (Day = 1; Day < WORKING_DAYS; Day++) {  
+        int Day=1;
+        while (Day <= WORKING_DAYS && Total_Working_Hours_Till_Now<= 100 ) {
+
             System.out.println(" Wage for Day " + Day);
             double empCheck = (Math.floor(Math.random() * 10) % 3);
             switch ((int) empCheck) {
@@ -18,7 +19,7 @@ public class Employeewage {
                     WorkHours = 8;
                     int PerdaySalary = EMP_WAGE_PER_HOUR * WorkHours;
                     Totalwage= Totalwage +PerdaySalary;
-
+                    Total_Working_Hours_Till_Now += WorkHours;
                     System.out.println("Full time Emp is " + PerdaySalary);
                     System.out.println("");
                 }
@@ -27,6 +28,7 @@ public class Employeewage {
                     WorkHours = 4;
                     int PerdaySalary = EMP_WAGE_PER_HOUR * WorkHours;
                     Totalwage= Totalwage +PerdaySalary;
+                    Total_Working_Hours_Till_Now += WorkHours;
 
                     System.out.println("Part time Emp is " + PerdaySalary);
                     System.out.println("");
@@ -36,18 +38,17 @@ public class Employeewage {
                     WorkHours = 0;
                     int PerdaySalary = EMP_WAGE_PER_HOUR * WorkHours;
                     Totalwage= Totalwage +PerdaySalary;
+                    Total_Working_Hours_Till_Now += WorkHours;
                     System.out.println("Absentee is " + PerdaySalary );
                     System.out.println("");
                 }
-
             }
-
+            Day++;
         }
-                    System.out.println("Total wage is "+Totalwage);
-	            
-	        
-
+        System.out.println( "Total worked hours are " + Total_Working_Hours_Till_Now);
+        System.out.println("Total wage is "+Totalwage);
+                }
     }
-	}
+	
 
 
